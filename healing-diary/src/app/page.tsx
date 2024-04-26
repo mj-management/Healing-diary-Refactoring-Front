ed0229067ea9bf4931b391a6281bab34d712174a
sangzun-han <hsjun1996@naver.com>
2024-04-26 19:11:20 +0900

    Chore : init nextjs, shadcn, tailwindcss

---
+++ healing-diary/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex w-full max-w-limit flex-1 flex-col items-stretch justify-start">
+      <h1>gd</h1>
+    </main>
+  );
+}
---
+++ healing-diary/src/common/header/header.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { NotionLogoIcon } from "@radix-ui/react-icons";
+import { AlarmCheck, BellIcon, SearchIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 z-50 w-full flex flex-col items-center justify-start">
+      <div className="h-[52px] max-h-[52px] relative z-50 flex w-full max-w-limit items-center justify-between gap-4">
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <span className="sr-only">Home</span>
+            <Image
+              src="/assets/images/icon.svg"
+              className="h-16 object-left pt-1"
+              alt="logo"
+              width={120}
+              height={120}
+            />
+          </Link>
+        </div>
+        <div className="flex justify-center">
+          <Button size="icon" variant="transparent">
+            <SearchIcon className="w-4 h-4" />
+          </Button>
+          <Button size="icon" variant="transparent">
+            <BellIcon className="w-4 h-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
