3acc48e62818993ecfe2be87c00dcabf61c55ce4
sangzun-han <hsjun1996@naver.com>
2024-05-01 21:09:03 +0900

    Feat : ui styled-compnent calendar

--- healing-diary/src/app/calendar/page.tsx
+++ healing-diary/src/app/calendar/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { ThemeProvider } from "styled-components";
 import DiaryCalendar from "@/components/calendar/diary-calendar";
+import theme from "@/theme";
 
 export default function CalendarPage() {
   return (
-    <main className="flex w-full max-w-limit flex-1 flex-col items-stretch justify-start bg-white">
-      <DiaryCalendar />
-    </main>
+    <ThemeProvider theme={theme}>
+      <main className="flex w-full max-w-limit flex-1 flex-col items-stretch justify-start bg-white">
+        <DiaryCalendar />
+      </main>
+    </ThemeProvider>
   );
 }
--- healing-diary/src/components/calendar/diary-calendar.tsx
+++ healing-diary/src/components/calendar/diary-calendar.tsx
@@ -1,7 +1,58 @@
+"use client";
+
+import { useState } from "react";
+import { StyledCalendarWrapper, StyledCalendar, StyledDate, StyledToday, StyledDot } from "./styles/styled-calendar";
+import moment from "moment";
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export default function DiaryCalendar() {
+  const today = new Date();
+  const [date, setDate] = useState<Value>(today);
+  const [activeStartDate, setActiveStartDate] = useState<Date | null>(new Date());
+  const attendDay = ["2024-05-01", "2024-05-03"]; // 출석한 날짜 예시
+
+  const handleDateChange = (newDate: Value) => {
+    setDate(newDate);
+  };
+
+  const handleTodayClick = () => {
+    const today = new Date();
+    setActiveStartDate(today);
+    setDate(today);
+  };
+
   return (
-    <div>
-      <h1>gd</h1>
-    </div>
+    <StyledCalendarWrapper>
+      <StyledCalendar
+        locale="ko"
+        value={date}
+        onChange={handleDateChange}
+        formatDay={(locale, date) => moment(date).format("D")}
+        formatYear={(locale, date) => moment(date).format("YYYY")}
+        formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
+        calendarType="gregory"
+        showNeighboringMonth={false}
+        next2Label={null}
+        prev2Label={null}
+        minDetail="year"
+        // 오늘 날짜로 돌아오는 기능을 위해 필요한 옵션 설정
+        activeStartDate={activeStartDate === null ? undefined : activeStartDate}
+        onActiveStartDateChange={({ activeStartDate }) => setActiveStartDate(activeStartDate)}
+        // 오늘 날짜에 '오늘' 텍스트 삽입하고 출석한 날짜에 점 표시를 위한 설정
+        tileContent={({ date, view }) => {
+          let html = [];
+          //   if (view === "month" && date.getMonth() === today.getMonth() && date.getDate() === today.getDate()) {
+          //     html.push(<StyledToday key={"today"}>오늘</StyledToday>);
+          //   }
+          if (attendDay.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
+            html.push(<StyledDot key={moment(date).format("YYYY-MM-DD")} />);
+          }
+          return <>{html}</>;
+        }}
+      />
+      <StyledDate onClick={handleTodayClick}>오늘</StyledDate>
+    </StyledCalendarWrapper>
   );
 }
